b8052f8fcaf25d8d710c56ef4ba96db385f5619c
TioSam989 <63099485+TioSam989@users.noreply.github.com>
2020-10-26 21:22:32 +0000

    Adicione função multiplicar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { add } = require("./utils");
+const { add, multiply } = require("./utils");
 
 const add2 = add(2);
 const a = add2(3);
+const multiplyByA = multiply(a);
+const b = multiplyByA(3);
 
-console.log(a);
+console.log(a, b);
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 const add = (x) => (y) => x + y
+const multiply = (x) => (y) => x * y
 
 module.exports = {
-    add
+    add,
+    multiply
 }
