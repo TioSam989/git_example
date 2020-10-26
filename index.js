7b9f53e518f16558ebfdd9c448a70893273f3366
TioSam989 <63099485+TioSam989@users.noreply.github.com>
2020-10-26 21:35:31 +0000

    Revert "Remova o console log (Luciano fez merda)"
    This reverts commit 1c70b295f316032d836cc1fd69c5e6ca1fe1b06c.

--- index.js
+++ index.js
@@ -5,8 +5,4 @@ const a = add2(3);
 const multiplyByA = multiply(a);
 const b = multiplyByA(2);
 
-/// TAR-123 - Luciano - 26/10/2020 - Adicionei essa linha
-/// TAR-124 - Luciano - 30/10/2020 - Removi linha
-/// console.log(a, b);
-/// TAR-124
-/// TAR-123
+console.log(a, b);
