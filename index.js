b04ad6b5339ab2a0efdf1030ca838a720de26aa9
TioSam989 <63099485+TioSam989@users.noreply.github.com>
2020-10-26 21:51:11 +0000

    ultimo comit desse arquiv

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const { add, multiply } = require("./utils");
 const add2 = add(2);
 const a = add2(3);
 const multiplyByA = multiply(a);
-const b = multiplyByA(6);
+const b = multiplyByA(4);
 
 console.log(a, b);
